eda4f102ef31ca91e54c89edaed6de736737fb7b
anothergreenhat <jamadera28@gmail.com>
2021-12-11 12:04:41 -0500

    working on plaintext argv's

--- catsay.c
+++ catsay.c
@@ -5,7 +5,7 @@
 #define MAXBUF 5000
 
 //CAT ASCII art images found on https://www.asciiart.eu/animals/cats
-//credit/signatures left in image originally containing them
+//credit/signatures left in images originally containing them
 
 void print_loop(char, int);
 
@@ -14,28 +14,44 @@ int main(int argc, char *argv[])
     srand(time(0));
     char catname[9] = {'c', 'a', 't', 0, '.', 't', 'x', 't', '\0' };
 
-    if(argc == 3 && argv[1][1] == 'w')
+    FILE *argstr = fopen("tmpargs","w+");
+    int opt_end = 0;
+    FILE *stream = stdin;
+    if(argv[1][0] == '-')
     {
-        if(argv[2][0] == '1' || argv[2][0] == '2' || argv[2][0] == '3')
-            catname[3] = argv[2][0];
+        if( argv[1][1] == 'w') 
+        {
+            if(argv[2][0] == '1' || argv[2][0] == '2' || argv[2][0] == '3')
+                catname[3] = argv[2][0];
+            else
+            {
+                fprintf(stderr, "invalid choice of which cat, cat%c.txt, random cat chosen\n", argv[2][0]);
+                catname[3] = (rand()%3 + 1) + '0'; // A = 1, B = 3
+            }
+        }    
         else
         {
-            fprintf(stderr, "invalid file choice cat%c.txt, random cat chosen\n", argv[2][0]);
+            if(argc > 1) {
+                fprintf(stderr, "unknown option: %s\n", argv[1]);
+                exit(1);
+            }
             catname[3] = (rand()%3 + 1) + '0'; // A = 1, B = 3
         }
+        opt_end++;
     }
-    else
+    if( argc >= 2)
     {
-        if(argc > 1)
-            fprintf(stderr, "unknown option: %s\n", argv[1]);
-        catname[3] = (rand()%3 + 1) + '0'; // A = 1, B = 3
+        for( int i = opt_end + 1; i < argc; i++)
+            fprintf(argstr,"%s",argv[i]);
+        stream = NULL;
+        stream = argstr;
     }
     FILE *fp = fopen(catname, "rb");
 
     char buf[MAXBUF] = {};
     int c, col = 0, max_col = 0, i = 0, tabcnt = 0;
 
-    while( (c = getchar()) != EOF)
+    while( (c = getc(stream)) != EOF)
     {
         buf[i] = c;
         if(c == '\t') { col = ((col + 8) / 8 * 8); tabcnt++; }
@@ -80,6 +96,8 @@ int main(int argc, char *argv[])
 
     while( (c = fgetc(fp)) != EOF) putchar(c);
     putchar('\n');
+    fclose(argstr);
+    remove("tmpargs");
     fclose(fp);
     return 0;
 }
